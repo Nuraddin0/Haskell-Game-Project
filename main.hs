4052e2b3eeb06590fdf7976a98c0282cfc0eec5d
Nureddin <nuraddinabbas@gmail.com>
2025-04-27 11:20:04 +0300

    Missing exception cases handled.

--- main.hs
+++ main.hs
@@ -10,8 +10,7 @@ main = do
     printBoard gameBoard
 
     putStrLn "Enter the maximum number of total moves allowed:"
-    input <- getLine
-    let maxMoves = read input :: Int
+    maxMoves <- askForMaxMoves
     let posOfA = 1
         posOfB = 5
         posOfC = 11
@@ -19,7 +18,7 @@ main = do
 
     putStrLn "Who starts first? Type 'last' or 'firsts':"
     startingSide <- askWhichToStart
-    
+
     let firstTurn = startingSide == "firsts"
         lastTurn = startingSide == "last"
 
@@ -44,16 +43,16 @@ startGame board maxMoves currentMoves firstTurn lastTurn posOfA posOfB posOfC po
                     else if letter == "B" then updateBoard board cell posOfB
                     else if letter == "C" then updateBoard board cell posOfC
                     else board -- never executes
-                moveCount = currentMoves + 1 
+                moveCount = currentMoves + 1
 
             printBoard newBoard
             putStrLn ("Completed moves: " ++ show moveCount)
-            
+
 
             let isGameEnded = if letter == "A" then checkWinCondition newBoard maxMoves moveCount cell posOfB posOfC posOfZ
                     else if letter == "B" then checkWinCondition newBoard maxMoves moveCount posOfA cell posOfC posOfZ
                     else if letter == "C" then checkWinCondition newBoard maxMoves moveCount posOfA posOfB cell posOfZ
-                    else checkWinCondition newBoard maxMoves moveCount posOfA posOfB posOfC posOfZ 
+                    else checkWinCondition newBoard maxMoves moveCount posOfA posOfB posOfC posOfZ
 
             if isGameEnded == 0 then putStrLn "DRAW!"
             else if isGameEnded == 1 then putStrLn "A&B&C WIN!"
@@ -62,7 +61,7 @@ startGame board maxMoves currentMoves firstTurn lastTurn posOfA posOfB posOfC po
                 if letter == "A" then startGame newBoard maxMoves moveCount False True cell posOfB posOfC posOfZ
                 else if letter == "B" then startGame newBoard maxMoves moveCount False True posOfA cell posOfC posOfZ
                 else if letter == "C" then startGame newBoard maxMoves moveCount False True posOfA posOfB cell posOfZ
-                else return()-- never executes
+                else return ()-- never executes
         else do
             putStrLn "Invalid move! Turn passes to opponent..."
             startGame board maxMoves currentMoves False True posOfA posOfB posOfC posOfZ
@@ -70,7 +69,7 @@ startGame board maxMoves currentMoves firstTurn lastTurn posOfA posOfB posOfC po
     else do
         putStrLn "Please select a cell for the Z:"
         cell <- askForLastMove
-        
+
         let isValidMove = isValidLast board cell posOfZ
         if isValidMove then do
             let newBoard = updateBoard board cell posOfZ
@@ -89,38 +88,57 @@ startGame board maxMoves currentMoves firstTurn lastTurn posOfA posOfB posOfC po
             putStrLn "Invalid move! Turn passes to opponent..."
             startGame board maxMoves currentMoves True False posOfA posOfB posOfC posOfZ
 
+askForMaxMoves :: IO Int
+askForMaxMoves = do
+    input <- getLine
+    if input == "" then do
+        putStrLn "Input can not be empty! Enter the maximum number of total moves allowed:"
+        askForMaxMoves
+    else do
+        case readMaybe input :: Maybe Int of
+            Nothing -> do
+                putStrLn "Invalid input type! Please enter an integer"
+                askForMaxMoves
+            Just maxMove -> return maxMove
+
 
 askForFirstsMove :: IO (String, Int)
 askForFirstsMove = do
     input <- getLine
-    let [letter, pos] = words input
-    
-    if (letter `elem` ["A","B","C"]) then do
-        case readMaybe pos :: Maybe Int of
-            Nothing -> do
-                putStrLn ("Invalid input! Please select one of first three letters and a cell to move it (e.g., A 6)")
-                askForFirstsMove
-            Just cell -> return (letter, cell)
-    else do
-        putStrLn ("Invalid input! Please select one of first three letters and a cell to move it (e.g., A 6)")
+    if input == "" then do
+        putStrLn "Input can not be empty! Please select one of first three letters and a cell to move it (e.g., A 6)"
         askForFirstsMove
+    else do
+        let [letter, pos] = words input
+        if letter `elem` ["A","B","C"] then do
+            case readMaybe pos :: Maybe Int of
+                Nothing -> do
+                    putStrLn "Invalid input! Please select one of first three letters and a cell to move it (e.g., A 6)"
+                    askForFirstsMove
+                Just cell -> return (letter, cell)
+        else do
+            putStrLn "Invalid input! Please select one of first three letters and a cell to move it (e.g., A 6)"
+            askForFirstsMove
 
 askForLastMove :: IO Int
 askForLastMove = do
     input <- getLine
-    case readMaybe input :: Maybe Int of
-        Nothing -> do
-            putStrLn ("Invalid input! Please select a cell for the Z:")
-            askForLastMove
-        Just cell -> return cell
-
+    if input == "" then do
+        putStrLn "Input can not be empty! Please select a cell for the Z:"
+        askForLastMove
+    else
+        case readMaybe input :: Maybe Int of
+            Nothing -> do
+                putStrLn "Invalid input! Please select a cell for the Z:"
+                askForLastMove
+            Just cell -> return cell
 
 
 checkWinCondition :: [Char] -> Int -> Int -> Int -> Int -> Int -> Int -> Int
 checkWinCondition board maxMoves moveCount posOfA posOfB posOfC posOfZ = do
     -- return 0 for DRAW, 1 for firstSideWins, 2 for lastSideWins, 3 for game is not ended
-    if ( (posOfZ `mod` 5) < (posOfA `mod` 5) && (posOfZ `mod` 5) < (posOfB `mod` 5) && (posOfZ `mod` 5) < (posOfC `mod` 5)) then 2 --Z wins
-    else if (not (any (\cell -> isValidLast board cell posOfZ) [posOfZ+1, posOfZ-1, posOfZ+5, posOfZ-5, posOfZ+6, posOfZ-6, posOfZ+4, posOfZ-4])) then 1
+    if ((posOfZ `mod` 5) < (posOfA `mod` 5) && (posOfZ `mod` 5) < (posOfB `mod` 5) && (posOfZ `mod` 5) < (posOfC `mod` 5)) then 2 --Z wins
+    else if (not (any (\cell -> isValidLast board cell posOfZ) [posOfZ+1, posOfZ-1, posOfZ+5, posOfZ-5, posOfZ+6, posOfZ-6, posOfZ+4, posOfZ-4])) then 1
     else if (maxMoves == moveCount) then 0 --Draw
     else 3 --Game is not ended
 
@@ -157,7 +175,10 @@ isValidFirst board cell pos
 askWhichToStart :: IO String
 askWhichToStart = do
     input <- getLine
-    if (input /= "firsts" && input /= "last") then do
-        putStrLn "Invalid input"
+    if input == "" then do
+        putStrLn "Input can not be empty! Who starts first? Type 'last' or 'firsts':"
+        askWhichToStart
+    else if input /= "firsts" && input /= "last" then do
+        putStrLn "Invalid input! Who starts first? Type 'last' or 'firsts':"
         askWhichToStart
     else return input
